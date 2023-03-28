8202c1c9edeb303112963a92690c73f02054ebd3
FedericoGre <federico.garea@gmail.com>
2023-03-28 18:43:28 -0300

    cambio a js

--- productManager.js
+++ productManager.js
@@ -120,7 +120,40 @@ class ProductManager {
 };
 
 
-export default ProductManager;
+const productManager= new ProductManager();
+
+productManager.addProducts("La Renga", "Banda de Rock", 200, "img", 10, "code123");
+productManager.addProducts("Los Redondos", "Banda de Rock", 400, "imgda", 110, "code1234");
+
+
+//con propiedades vacias
+productManager.addProducts("Soda-Stereo");
+
+
+// con code repetido
+productManager.addProducts("Los Piojos", "Banda de Rock", 190, "img", 10, "code123");
+
+
+
+//code repetido y con vacios
+
+productManager.addProducts("Los Piojos", undefined, 190, "img", 10, "code123");
+
+
+//muestra la lista de productos
+console.log(productManager.getProducts());
+
+
+//busqueda de id inexistente
+console.log(productManager.getProductById(46));
+
+//busqueda de id existente
+
+console.log(productManager.getProductById(1));
+
+
+
+
 
 
 
